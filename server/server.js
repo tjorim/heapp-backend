ab9cd9af3fdb18364cc7a29ff352247152e05011
Rob De Boever <rob.deboever@hotmail.com>
2019-03-01 23:14:53 +0100

    fixed pathing

--- server/server.js
+++ server/server.js
@@ -1,25 +1,23 @@
 const express = require('express');
-const routes = require('./routes/index');
-
+const routes = require('./../routes/index');
+var path = require('path');
 
 //Database
 const mongoose = require('mongoose');
 
 
+const app = require('./../app');
 
+const server = app.listen(3000, () => {
+  console.log(`Express is running on port ${server.address().port}`);
+});
 
-const app = express();
-app.use('/', routes);
-
-module.exports = app;
-
-
-mongoose.connect(process.env.DATABASE, { useMongoClient: true });
-mongoose.Promise = global.Promise;
-mongoose.connection
-  .on('connected', () => {
-    console.log(`Mongoose connection open on ${process.env.DATABASE}`);
-  })
-  .on('error', (err) => {
-    console.log(`Connection error: ${err.message}`);
-  });
+// mongoose.connect(process.env.DATABASE, { useMongoClient: true });
+// mongoose.Promise = global.Promise;
+// mongoose.connection
+//   .on('connected', () => {
+//     console.log(`Mongoose connection open on ${process.env.DATABASE}`);
+//   })
+//   .on('error', (err) => {
+//     console.log(`Connection error: ${err.message}`);
+//   });
